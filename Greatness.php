528d09b337519c228bebabca4b53b36b74bdc1a8
demarim <demarim@umich.edu>
2016-08-13 18:03:23 -0400

    Add files via upload

---
+++ Greatness.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>The Greatness Camp</title>
+	<meta charset="utf-8"/>
+
+	<link href="colors.css" type="text/css" rel="stylesheet"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body class="body">
+<?php
+phpinfo();
+echo "What are you doing today";
+?>
+
+	<header class="mainHeader">
+		<img align="center" height="100px" src="logo.png"/>
+
+		<nav><ul>
+			<li class="active"><a href="#">Home</a></li>
+			<li><a href="#">About GC</a></li>
+			<li><a href="#">Professional Developments</a></li>
+			<li><a href="#">Events</a></li>
+			<li><a href="#">Gallery</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul></nav>
+	</header>
+
+	<div class="mainContent">
+		<div class="Content">
+			<article class="topContent">
+				<header>
+					<h2><a href="#" title="First Post">First Post</a></h2>
+				</header>
+
+				<footer>
+					<p class="post-info">PROMOTING GREATNESS ONE STUDENT AT A TIME!</p>
+				</footer>
+
+				<content>
+					<p>
+					Sociable on as carriage my position weddings raillery consider. Peculiar trifling absolute and wandered vicinity property yet. The and collecting motionless difficulty son. His hearing staying ten colonel met. Sex drew six easy four dear cold deny. Moderate children at of outweigh it. Unsatiable it considered invitation he travelling insensible. Consulted admitting oh mr up as described acuteness propriety moonlight. 
+					</p>
+					<p>
+					Sociable on as carriage my position weddings raillery consider. Peculiar trifling absolute and wandered vicinity property yet. The and collecting motionless difficulty son. His hearing staying ten colonel met. Sex drew six easy four dear cold deny. Moderate children at of outweigh it. Unsatiable it considered invitation he travelling insensible. Consulted admitting oh mr up as described acuteness propriety moonlight. </p>
+
+				</content>
+
+			</article>
+
+		<article class="bottomContent">
+				<header>
+					<h2><a href="#" title="Second Post">Second Post</a></h2>
+				</header>
+
+				<footer>
+					<p class="post-info">PROMOTING GREATNESS ONE STUDENT AT A TIME!</p>
+				</footer>
+
+				<content>
+					<p>
+					Sociable on as carriage my position weddings raillery consider. Peculiar trifling absolute and wandered vicinity property yet. The and collecting motionless difficulty son. His hearing staying ten colonel met. Sex drew six easy four dear cold deny. Moderate children at of outweigh it. Unsatiable it considered invitation he travelling insensible. Consulted admitting oh mr up as described acuteness propriety moonlight. 
+					</p>
+					<p>
+					Sociable on as carriage my position weddings raillery consider. Peculiar trifling absolute and wandered vicinity property yet. The and collecting motionless difficulty son. His hearing staying ten colonel met. Sex drew six easy four dear cold deny. Moderate children at of outweigh it. Unsatiable it considered invitation he travelling insensible. Consulted admitting oh mr up as described acuteness propriety moonlight. </p>
+
+				</content>
+
+			</article>
+		</div>
+	</div>
+
+	<aside class="top-sidebar">
+		<article>
+			<h2>Top Sidebar</h2>
+				<p>Unfeeling so rapturous discovery he exquisite. Reasonably so middletons or impression by terminated. Old pleasure required removing elegance him had. Down she bore sing saw calm high. Of an or game gate west face shed. ﻿no great but music too old found arose. 
+				</p>
+
+		</article>
+	</aside>
+
+	<aside class="middle-sidebar">
+		<article>
+			<h2>Middle Sidebar</h2>
+				<p>Unfeeling so rapturous discovery he exquisite. Reasonably so middletons or impression by terminated. Old pleasure required removing elegance him had. Down she bore sing saw calm high. Of an or game gate west face shed. ﻿no great but music too old found arose. 
+				</p>
+
+		</article>
+	</aside>
+
+	<aside class="bottom-sidebar">
+		<article>
+			<h2>Bottom Sidebar</h2>
+				<p>Unfeeling so rapturous discovery he exquisite. Reasonably so middletons or impression by terminated. Old pleasure required removing elegance him had. Down she bore sing saw calm high. 
+				</p>
+
+		</article>
+	</aside>
+
+	<footer class="mainfooter">
+		<p>Copyright &copy; <a href="#" title="thegcamp.com">thegcamp.com</a></p>
+	</footer>
+</body>
+
+</html>
